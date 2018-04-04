e55edaabbc5131633eec87681cb820f66681e67c
jenkin-du <jenkin.ahorn@gmail.com>
2018-04-04 17:58:24 +0800

    fix some problem

--- ArcPy_study/GUI_Test.py
+++ ArcPy_study/GUI_Test.py
@@ -35,33 +35,83 @@
 # app.master.title("title")
 # app.mainloop()
 
-import sys
+# import sys
+#
+# from PyQt4 import QtGui
+#
+#
+# class Icon(QtGui.QWidget):
+#     def __init__(self, parent=None):
+#         QtGui.QWidget.__init__(self, parent)
+#
+#         self.setGeometry(100, 100, 450, 350)
+#         self.setWindowTitle('this is a window')
+#         self.setWindowIcon(QtGui.QIcon('icons/app.png'))
+#         # 设置气泡提示
+#         self.setWindowTitle('Tooltip')
+#
+#         self.setToolTip('This is a <b>QWidget</b> widget')
+#         # QtGui.QToolTip.setFont(QtGui.QFont('OldEnglish', 10))
+#
+#
+# app = QtGui.QApplication(sys.argv)
+# #
+# # widget = QtGui.QWidget()
+# # widget.resize(800, 600)
+# # widget.setWindowTitle('simple')
+# # widget.show()
+#
+# icon = Icon()
+# icon.show()
+#
+# sys.exit(app.exec_())
 
+
+# import sys
+# from PyQt4 import QtGui, QtCore
+#
+# class QuitButton(QtGui.QWidget):
+#     def __init__(self, parent=None):
+#         QtGui.QWidget.__init__(self, parent)
+#
+#         self.setGeometry(300, 300, 250, 150)
+#         self.setWindowTitle('Quit button')
+#
+#         quit = QtGui.QPushButton('Close', self)
+#         quit.setGeometry(10, 10, 64, 35)
+#
+#         self.connect(quit, QtCore.SIGNAL('clicked()'),
+#             QtGui.qApp, QtCore.SLOT('quit()'))
+#
+#
+# app = QtGui.QApplication(sys.argv)
+# qb = QuitButton()
+# qb.show()
+# sys.exit(app.exec_())
+
+import sys
 from PyQt4 import QtGui
 
 
-class Icon(QtGui.QWidget):
+class MessageBox(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self.setGeometry(100, 100, 450, 350)
-        self.setWindowTitle('this is a window')
-        self.setWindowIcon(QtGui.QIcon('icons/app.png'))
-        # 设置气泡提示
-        self.setWindowTitle('Tooltip')
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('message box')
 
-        self.setToolTip('This is a <b>QWidget</b> widget')
-        # QtGui.QToolTip.setFont(QtGui.QFont('OldEnglish', 10))
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message',
+                                           "Are you sure to quit?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 app = QtGui.QApplication(sys.argv)
-#
-# widget = QtGui.QWidget()
-# widget.resize(800, 600)
-# widget.setWindowTitle('simple')
-# widget.show()
+qb = MessageBox()
 
-icon = Icon()
-icon.show()
-
+qb.show()
 sys.exit(app.exec_())
